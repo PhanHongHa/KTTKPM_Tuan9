aa0e08194f5341995b711a825b48688a11dfa6d7
ui2 <phanhongha96@gmail.com>
2022-11-04 14:12:37 +0700

    Spring boot redis crud

---
+++ src/main/java/com/example/SpringBootRedisCRUD/SpringBootRedisCrudApplication.java
@@ -0,0 +1,29 @@
+package com.example.SpringBootRedisCRUD;
+
+import com.example.SpringBootRedisCRUD.entity.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+@EnableCaching
+public class SpringBootRedisCrudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRedisCrudApplication.class, args);
+  }
+  @Bean
+  JedisConnectionFactory jedisConnectionFactory(){
+    return new JedisConnectionFactory();
+  }
+
+  @Bean
+  RedisTemplate<String, Item> redisTemplate(){
+    RedisTemplate<String,Item> redisTemplate = new RedisTemplate<String, Item>();
+    redisTemplate.setConnectionFactory(jedisConnectionFactory());
+    return redisTemplate;
+  }
+}
